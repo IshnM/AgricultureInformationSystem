a45e1913baa433dbd2f21fff6dbf1ea54ab2f76b
Ishan Madhawa <ishanmadhawa44@outlook.com>
2020-04-16 01:10:50 +0530

    Database Linking

--- AgricultureInformationSystem/AgricultureInformationSystem/__init__.py
+++ AgricultureInformationSystem/AgricultureInformationSystem/__init__.py
@@ -3,6 +3,14 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '\x81I\xcb\xef\x10\xbb\xe5\xa5\xfdP\x17_\xdf2\x10\xe2mUv\xf1\x01_\xc8\x05\x85}\xe6\xe0M\xcb\xb1+'
+app.config["SQLALCHEMY_DATABASE_URL"]='postgresql://postgres:123@localhost:5432/Aginfo'
+db=SQLAlchemy(app)
+
+
 
 import AgricultureInformationSystem.views
+
--- AgricultureInformationSystem/AgricultureInformationSystem/views.py
+++ AgricultureInformationSystem/AgricultureInformationSystem/views.py
@@ -6,6 +6,10 @@ from datetime import datetime
 from flask import render_template
 from AgricultureInformationSystem import app
 
+
+
+
+
 @app.route('/')
 @app.route('/home')
 def home():
