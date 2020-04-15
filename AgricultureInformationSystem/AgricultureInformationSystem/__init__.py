298ba4b202584cce30e73a462325b389b03ed1ce
Ishan Madhawa <ishanmadhawa44@outlook.com>
2020-04-16 01:27:41 +0530

    linking_database

--- AgricultureInformationSystem/AgricultureInformationSystem/__init__.py
+++ AgricultureInformationSystem/AgricultureInformationSystem/__init__.py
@@ -6,7 +6,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+#SECREAT KEY
 app.config['SECRET_KEY'] = '\x81I\xcb\xef\x10\xbb\xe5\xa5\xfdP\x17_\xdf2\x10\xe2mUv\xf1\x01_\xc8\x05\x85}\xe6\xe0M\xcb\xb1+'
+#Database URL
 app.config["SQLALCHEMY_DATABASE_URL"]='postgresql://postgres:123@localhost:5432/Aginfo'
 db=SQLAlchemy(app)
 
